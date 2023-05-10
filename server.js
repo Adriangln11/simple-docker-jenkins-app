8c4145dd261226e6cc575a68aadabd6c85c100a9
AdrianDiaz <cesarqqu@gmail.com>
2023-05-09 23:05:21 -0600

    new version

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ import express from 'express'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World, this is the new version!')
 })
 app.listen(3000, console.log(`Listening on port: 3000`))
